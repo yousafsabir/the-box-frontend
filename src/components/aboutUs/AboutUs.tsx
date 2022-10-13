5ccdd518ea30dc8da9c64e4ca8724e9a194cc8c8
yousafsabir <yousafsabir430516@gmail.com>
2022-10-13 12:09:19 +0500

    about us files created

---
+++ src/components/aboutUs/AboutUs.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./AboutUs.scss";
+
+const AboutUs: React.FC = () => {
+    return (
+        <section>
+            <div className="container"></div>
+        </section>
+    );
+};
+
+export default AboutUs;
